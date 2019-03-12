5026dd416c0e7779b4398cfb2abeadb1c9b9ce2f
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-12 09:20:02 -0700

    imported image to js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const canvas = document.querySelector('canvas');
+const img = document.createElement('img');
+img.src = 'img/sprite.png';
 canvas.width = '500';
-canvas.height = '500';
+canvas.height='500';
+
+const context = canvas.getContext('2d');
+function gameLoop(){
+     context.drawImage(img,0,0);
+     window.requestAnimationFrame(gameLoop);
+
+}
+gameLoop();
